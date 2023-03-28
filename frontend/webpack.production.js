7e4701b64f012c03334a0927cfd8b438f3c26434
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-03-28 20:40:05 +0500

    fix(webpack): pass envs

--- frontend/webpack.production.js
+++ frontend/webpack.production.js
@@ -8,7 +8,9 @@ module.exports = merge(common, {
     mode: "production",
     plugins: [
         new webpack.DefinePlugin({
+            'process.env.BACKEND_GATEWAY': JSON.stringify(process.env.BACKEND_GATEWAY),
             'process.env.PIPEDRIVE_CREATE_MODAL_ID': JSON.stringify(process.env.PIPEDRIVE_CREATE_MODAL_ID),
+            'process.env.PIPEDRIVE_EDITOR_MODAL_ID': JSON.stringify(process.env.PIPEDRIVE_EDITOR_MODAL_ID),
         }),
     ],
 });
