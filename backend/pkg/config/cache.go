0d59fe8ceede0da4715fbd7c2cd5cf27385bf237
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-03-30 14:02:30 +0500

    fix(caching): pass db as a config field

--- backend/pkg/config/cache.go
+++ backend/pkg/config/cache.go
@@ -33,6 +33,7 @@ type CacheConfig struct {
 		Size     int    `yaml:"size" env:"CACHE_SIZE,overwrite"`
 		Address  string `yaml:"address" env:"CACHE_ADDRESS,overwrite"`
 		Password string `yaml:"password" env:"CACHE_PASSWORD,overwrite"`
+		DB       int    `yaml:"db" env:"CACHE_DB,overwrite"`
 	} `yaml:"cache"`
 }
 
