dee6e0acd69052cc3cf03bc33e5ee1949101d647
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-05-10 18:30:43 +0500

    fix(pages): settings page overflow

--- frontend/src/pages/Settings/index.tsx
+++ frontend/src/pages/Settings/index.tsx
@@ -113,7 +113,7 @@ export const SettingsPage: React.FC = () => {
   };
 
   return (
-    <div className="w-screen h-screen overflow-scroll">
+    <div className="w-screen h-screen overflow-y-scroll overflow-x-hidden">
       {loading && !error && (
         <div className="h-full w-full flex justify-center items-center">
           <OnlyofficeSpinner />
