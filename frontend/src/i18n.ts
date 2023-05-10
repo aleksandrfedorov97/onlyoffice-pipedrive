7ed7142a85eab95440acb2cd17ee37a38180b55f
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-05-10 17:34:22 +0500

    fix(i18n): remove autodetect

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -18,11 +18,9 @@
 
 import i18n from "i18next";
 import I18NextHttpBackend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 i18n
-  .use(LanguageDetector)
   .use(I18NextHttpBackend)
   .use(initReactI18next)
   .init({
